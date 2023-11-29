5de747d14a1ab5580ad67840512bf3a63baa3eb7
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:08:33 +0100

    .

--- Vector_Iteradores.cpp
+++ Vector_Iteradores.cpp
@@ -5,3 +5,11 @@ int main() {
     std::vector<int> numeros = {10, 20, 30, 40, 50};
 
     std::cout << "Iterando a través del vector de números:" << std::endl;
+    for(const auto& numero : numeros) {
+        std::cout << "- " << numero << std::endl;
+    }
+
+    std::cout << "Fin de la iteración." << std::endl;
+
+    return 0;
+}
