c0cd1f01d89ac42c401f109c9923996a74d8e632
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:20:56 +0100

    .

--- Calculo_Numerico.cpp
+++ Calculo_Numerico.cpp
@@ -4,3 +4,7 @@
 int main() {
     std::valarray<double> valores = {1.0, 2.0, 3.0, 4.0, 5.0};
     std::cout << "Operaciones con Valarray:" << std::endl;
+    std::cout << "Suma: " << valores.sum() << std::endl;
+    std::cout << "Media: " << valores.sum() / valores.size() << std::endl;
+    valores += 5.0;
+    std::cout << "Valores + 5.0: ";
