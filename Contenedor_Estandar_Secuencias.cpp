cf62f4ab2ddb49bcd498ec4eb67aeda40181fae8
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:11:49 +0100

    .

--- Contenedor_Estandar_Secuencias.cpp
+++ Contenedor_Estandar_Secuencias.cpp
@@ -5,5 +5,15 @@
 int main() {
     std::list<float> lista = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::stack<float, std::list<float>> pila(lista);
+    std::cout << "Contenido de la pila (en orden inverso):" << std::endl;
+
+    while (!pila.empty()) {
+        std::cout << "- Elemento en la cima: " << pila.top() << std::endl;
+        pila.pop();
+    }
+    std::cout << "Fin del proceso de la pila." << std::endl;
+
+    return 0;
+}
 
 
