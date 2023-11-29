0861bbcf7e10bc0fab1e5617d58793ce9e3b95f7
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:16:26 +0100

    .

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -5,5 +5,11 @@
 int main() {
     std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
     std::sort(numeros.begin(), numeros.end());
-    
+    std::cout << "Números ordenados de manera ascendente:" << std::endl;
+
+    for (int num : numeros) {
+        std::cout << "- " << num << std::endl;
+    }
+    int maxNum = *std::max_element(numeros.begin(), numeros.end());
+
 
