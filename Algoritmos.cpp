41f7ce0f244b167efc5fe054da98bac838d24ce1
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:15:43 +0100

    .

---
+++ Algoritmos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
+    std::sort(numeros.begin(), numeros.end());
+    
+
