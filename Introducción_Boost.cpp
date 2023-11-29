a4c96213a6dde555ab88d3aea01182be935fe49a
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:51:56 +0100

    .

---
+++ Introducción_Boost.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+int main() {
+    std::string s = "12345";
+    std::cout << "Conversión de cadena a número usando Boost.LexicalCast:" << std::endl;
+    try {
+        int i = boost::lexical_cast<int>(s);
+
+        std::cout << "Número: " << i << std::endl;
+
+    } catch (const boost::bad_lexical_cast& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
