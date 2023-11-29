6f0a65f802099ca3531a4036d58e425ad191d165
JorgeRomojaro11 <jromodel@myuax.com>
2023-11-29 20:28:23 +0100

    .

--- C++_Moderno.cpp
+++ C++_Moderno.cpp
@@ -6,3 +6,10 @@
 auto cuadrado = [](int num) { return num * num; };
 
 int main() {
+    std::cout << "Cuadrado de 5: " << cuadrado(5) << std::endl;
+    std::cout << "Números en el vector:" << std::endl;
+    std::vector<int> numeros = {1, 2, 3, 4, 5};
+    std::for_each(numeros.begin(), numeros.end(), [](int num) {
+        std::cout << "- " << num << std::endl;
+    });
+}
